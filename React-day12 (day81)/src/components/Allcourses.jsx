1037376498784f2f25eceb2e5ceb36a432cfc662
Harshpatel-2510 <patelharsh251005@gmail.com>
2026-06-24 16:50:59 +0530

    New lecture files

---
+++ React-day12 (day81)/src/components/Allcourses.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Courses from './Courses';
+
+const Allcourses = () => {
+  return (
+    <div>
+        Allcourses
+        <Courses />
+        <Courses />
+        <Courses />
+        <Courses />
+    </div>
+  )
+}
+
+export default Allcourses
